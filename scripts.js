953dd6ab15cae4eaee44028ddfd33d95cb839166
chemonique <122254404+chemonique@users.noreply.github.com>
2023-05-22 09:10:14 +0200

    time,date change

--- scripts.js
+++ scripts.js
@@ -67,9 +67,17 @@ const data = {
 
 /*function*/ 
 const createHtml = (athlete) => {
+  console.log('athlete',athlete)
   //destruct
   const {firstName, surname, id, races }= athlete; 
-  const [date,time]= races.reverse();
+  console.log('firstnam',firstName)
+  console.log('races',races)
+
+  const lastRace= races.pop();
+  const {date,time}=lastRace
+  console.log('time',time)
+  const timeReverse = time.reverse();
+  console.log('timeR',timeReverse)
 
   //fragment
   const fragment =document.createDocumentFragment();
@@ -78,38 +86,38 @@ const createHtml = (athlete) => {
     title.textContent=id;
     fragment.appendChild(title);
 
-  //   //list create 
-  //   const list = document.createElement('dl');
+    //list create 
+    const list = document.createElement('dl');
     
-  //   // date 
-  //   const day = new Date().getDate();
-  //   const month = MONTHS[new Date().date];
-  //   const year =  new Date().getFullYear();
+    // date 
+    const day = new Date().getDate();
+    const month = MONTHS[new Date().date];
+    const year =  new Date().getFullYear();
 
-  //   // time
+    // time
     
-  //  //const [first, second, third, fourth] = time;
-  //  //const total = first + second + third + fourth;
-  //   const total=91;
-  //   const rhours =total / 60;
-  //   const hours = Math.floor(rhours);
-  //   const minutes = Math.round((rhours - hours) * 60);
+   const [first, second, third, fourth] = time;
+   const total = first + second + third + fourth;
+    //const total=91;
+    const rhours =total / 60;
+    const hours = Math.floor(rhours);
+    const minutes = Math.round((rhours - hours) * 60);
 
     
-  //   list.innerHTML = /* html */ `
-  //   <dt>Athlete</dt>
-  //   <dd>${firstName} ${surname}</dd>
+    list.innerHTML = /* html */ `
+    <dt>Athlete</dt>
+    <dd>${firstName} ${surname}</dd>
 
-  //   <dt>Total Races</dt>
-  //   <dd>${races.length    }</dd>
+    <dt>Total Races</dt>
+    <dd>${races.length    }</dd>
 
-  //   <dt>Event Date (Latest)</dt>
-  //   <dd>${day} ${month} ${year}</dd>
+    <dt>Event Date (Latest)</dt>
+    <dd>${day} ${month} ${year}</dd>
 
-  //   <dt>Total Time (Latest)</dt>
-  //   <dd>${hours.toString().padStart(2, 0) }:${minutes.toString().padStart(2, 0)}</dd>`;
+    <dt>Total Time (Latest)</dt>
+    <dd>${hours.toString().padStart(2, 0) }:${minutes.toString().padStart(2, 0)}</dd>`;
     
-  //   fragment.appendChild(list);
+    fragment.appendChild(list);
    
 
 }
@@ -120,6 +128,6 @@ const {NM372,SV782}= data.response.data;
 /*calling function */
 //createHtml(SV782);
 
-document.querySelector('[data-athlete="NM372"]').innerHTML=createHtml(NM372);
-document.querySelector('[data-athlete="SV782"]').innerHTML=createHtml(SV782);
+document.querySelector('[data-athlete="NM372"]').appendChild(createHtml(NM372));
+document.querySelector('[data-athlete="SV782"]').appendChild(createHtml(SV782));
 
